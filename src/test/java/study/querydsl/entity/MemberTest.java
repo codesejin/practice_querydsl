fd9f49bd4ef9beb3c49a454e14d8b52df382d081
codesejin <seijin0722@naver.com>
2023-03-08 15:35:20 +0900

    start - JPQL vs Querydsl

---
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,55 @@
+package study.querydsl.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        //초기화
+        em.flush(); // 영속성 컨텍스트에 있는 오브젝트들을 실제 쿼리를 만들어서 디비에 날리게 되고
+        em.clear(); // 영속성 컨텍스트를 초기화해서 캐시같은게 다 날라감
+
+        //확인  - Jpql
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.team" + member.getTeam());
+
+        }
+    }
+
+}
---
+++ src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -0,0 +1,88 @@
+package study.querydsl.entity;
+
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        //초기화
+        em.flush(); // 영속성 컨텍스트에 있는 오브젝트들을 실제 쿼리를 만들어서 디비에 날리게 되고
+        em.clear(); // 영속성 컨텍스트를 초기화해서 캐시같은게 다 날라감
+
+        //확인  - Jpql
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.team" + member.getTeam());
+
+        }
+    }
+
+    @Test
+    public void startJPQL() {
+        // member1.을 찾아라.
+//        Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
+        String qlString =
+                "select m from Member m " +
+                "where m.username = :username";
+
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl(){
+        QMember m = new QMember("m");
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1")) // 파라미터 바인딩 처리
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+}
