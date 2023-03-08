be135b39d8f70445f0371f9d56da5646ddf5b7cb
codesejin <seijin0722@naver.com>
2023-03-08 15:12:29 +0900

    commit : first init

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,58 @@
+package study.querydsl.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this(username,0);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String usernae, int age, Team team) {
+        this.username = usernae;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
