86218b38d80a2e758680957488675400e3f36d3c
codesejin <seijin0722@naver.com>
2023-03-08 17:37:14 +0900

    JOIN - Basic join

--- src/test/java/study/querydsl/entity/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -242,7 +242,45 @@ public class QuerydslBasicTest {
         Assertions.assertThat(teamA.get(member.age.avg())).isEqualTo(15); // ( 10 + 20 ) / 2
         Assertions.assertThat(teamB.get(team.name)).isEqualTo("teamB");
         Assertions.assertThat(teamB.get(member.age.avg())).isEqualTo(35); // ( 30 + 40 ) / 2
+    }
 
+    /**
+     * 팀 A에 소속된 모든 회원
+     * @throws Exception
+     */
+    @Test
+    public void join() throws Exception {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
 
+        Assertions.assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /**
+     * 세타 조인
+     * 회원의 이름이 팀 이름과 같은 히원 조회 ( 연관관계 없는 조인을 해보려고 함 ) 
+     * left outer join이나 right outer join 과 같은 외부 조인 불가능 -> hibernate 최신버저 들어가면서 조인 ON을 사용하면 외부 조인 가능
+     * @throws Exception
+     */
+    @Test
+    public void theta_join() throws Exception {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team) // 멤버랑 팀 테이블 전부 다 조인
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        Assertions.assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
     }
 }
