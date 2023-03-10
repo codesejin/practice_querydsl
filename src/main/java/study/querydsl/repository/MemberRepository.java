f2b15072feb54458be7bc47e4cd93ad0ecadc403
codesejin <seijin0722@naver.com>
2023-03-10 10:56:47 +0900

    사용자 정의 리포지토리

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import study.querydsl.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
     // spring data jpa가 메소드 이름으로 자동으로 JPQL을 만드는 전략이 있음
     // select m from Member m where m.username = ?
     List<Member> findByUsername(String username);
